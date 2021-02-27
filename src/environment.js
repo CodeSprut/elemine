195974c93a5ba98299dbccaa1fb198b194e3126e
CodeSprut <Alex-p.s@yandex.ru>
2021-02-27 21:54:24 +0500

    set portable exec path

--- src/environment.js
+++ src/environment.js
@@ -1,8 +1,12 @@
 const OS = require("os");
 const path = require("path");
-const execPath = process.execPath.match(/.*\\/i)[0];
+const isPortable = process.env.PORTABLE_EXECUTABLE_DIR !== undefined;
+const execPath = isPortable
+  ? `${process.env.PORTABLE_EXECUTABLE_DIR}\\`
+  : process.execPath.match(/.*\\/i)[0];
 
 module.exports = {
+  isPortable,
   execPath,
   appRoot: path.resolve(__dirname),
   devMode: process.argv[1] === ".",
